347c73899a7afb36e3dbb413be58f6a11e349fe9
Anant <p.anant223@gmail.com>
2025-05-09 23:20:29 +0530

    adding config in db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,9 +4,20 @@ import mongoose from "mongoose"
 const connectDB = async () =>{
     try {
         const connectionInstance = await mongoose.connect(
-            `${process.env.MONGODB_URI}/eventopia`
+            `${process.env.MONGODB_URI}/${DB_NAME}`,
+            {
+                serverSelectionTimeoutMS: 3000,
+                socketTimeoutMS: 30000,
+                maxPoolSize: 50,
+                minPoolSize: 5,
+                retryWrites: true,
+                retryReads: true,
+                writeConcern: {
+                    w: "majority",
+                    j: true,
+                },
+            }
         );
-        console.log("hosdf", process.env.MONGODB_URI);
         console.log(`\n✅ MongoDB Connected || DB Host : ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB connection has been failed");
