0937ad04bcf6f0a299684597057d994c7883f5e6
Anant <p.anant223@gmail.com>
2026-07-14 02:33:59 +0530

    add ticketmaster middelware

--- src/app.js
+++ src/app.js
@@ -55,6 +55,10 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+app.get("/api/v1/health", (req, res) => {
+    res.status(200).json({ status: "ok" });
+});
+
 import userRouter from "./routes/user.routes.js";
 import eventRouter from "./routes/event.routes.js";
 import likeRouter from "./routes/like.routes.js";
@@ -66,12 +70,15 @@ import locationRouter from "./routes/location.routes.js";
 // import categoryRouter from "./routes/category.routes.js";
 // import onBoardingRouter from "./routes/stripeOnboarding.routes.js"
 import waitlistRouter from "./routes/waitlist.routes.js"
+import otherRouter from "./routes/other.routes.js";
+
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/events", eventRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/register", registerRouter);
 app.use("/api/v1/google", locationRouter);
+app.use("/api/v1/other", otherRouter);
 // app.use("/api/v1/payments", pyamentRouter);
 // app.use("/api/v1/booking", bookingRouter)
 // app.use("/api/v1/notification", notificationRouter);
